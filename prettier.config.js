7e960dc03f24296953da3128b2b001eea1d445fb
nyx <arnaufs8@gmail.com>
2025-07-11 16:15:38 +0200

    Add testing and linter

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf',
+};
